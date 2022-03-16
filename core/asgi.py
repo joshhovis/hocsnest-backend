cbf1f42d9d743bbfbd0fbbbc2eb4155d38a56d65
sam-cowing <sam.ryan.cowing@gmail.com>
2022-03-15 23:49:37 -0400

    core.routing asgi updates

--- core/asgi.py
+++ core/asgi.py
@@ -1,13 +1,13 @@
 import os
 import django
-#from django.core.asgi import get_asgi_application
+from django.core.asgi import get_asgi_application
 from channels.routing import get_default_application
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-#application = get_asgi_application()
+application = get_asgi_application()
 
 #django.setup()
 
-application = get_default_application
+#application = get_default_application
--- core/routing.py
+++ core/routing.py
@@ -1,11 +1,10 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from .asgi import get_default_application
+from .asgi import get_asgi_application
 import chat.routing
 from chat.channelsmiddleware import JWTAuthMiddlewareStack
 
 application = ProtocolTypeRouter({
-    "https": get_default_application(),
     'websocket': JWTAuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
